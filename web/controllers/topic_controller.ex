26bfada22025095e075ef4f9c569fe0e6e987471
daneb <dane.balia@hetzner.co.za>
2018-02-14 13:07:50 +0200

    Added update for discussions

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,6 +3,13 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    # Discuss.Top
+    topics = Repo.all(Topic)
+    # Make topics available in the view
+    render conn, "index.html", topics: topics
+  end
+
     # No inheritance or class hierarchies
   # Alternatives:
 
@@ -30,17 +37,37 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect (post)
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
       {:error, changeset} ->
         render conn, "new.html", changeset: changeset
     end
-
   end
 
-  def index(conn, _params) do
-    # Discuss.Top
-    topics = Repo.all(Topic)
-    # Make topics available in the view
-    render conn, "index.html", topics: topics
+  def edit(conn, %{"id" => topic_id }) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{ "id" => topic_id, "topic" => topic }) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+
+    changeset = Repo.get(Topic, topic_id) |> Topic.changeset(topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset, topic: old_topic
+    end
+
+    render conn, "update.html", changeset: changeset, topic: topic
   end
 end
